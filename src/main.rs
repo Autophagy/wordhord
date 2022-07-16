33ba87f7672a78dc86438c7b09183ded3a22b9c8
Mika Naylor <mail@autophagy.io>
2022-07-16 19:51:19 +0200

    Add a tags page with links to posts

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,43 @@ use comrak::{markdown_to_html, ComrakOptions};
 use serde::{Deserialize, Serialize};
 use std::env;
 use std::error::Error;
+use std::fmt;
 use std::fs;
 use std::path::Path;
+use std::slice::Iter;
 use tinytemplate::{format_unescaped, TinyTemplate};
 
-#[derive(Clone, Deserialize, Serialize, Debug)]
-enum Tag {
+#[derive(Clone, Deserialize, Serialize, Debug, PartialEq)]
+pub enum Tag {
     A,
     B,
     C,
     D,
+}
+
+impl Tag {
+    pub fn iterator() -> Iter<'static, Tag> {
+        static TAGS: [Tag; 4] = [Tag::A, Tag::B, Tag::C, Tag::D];
+        TAGS.iter()
+    }
+}
+
+impl fmt::Display for Tag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let tag = match self {
+            Tag::A => "A",
+            Tag::B => "B",
+            Tag::C => "C",
+            Tag::D => "D",
+        };
+        write!(f, "{}", tag)
+    }
+}
+
+#[derive(Clone, Serialize, Debug)]
+struct TagPage {
+    tag: Tag,
+    posts: Vec<Post>,
 }
 
 #[derive(Clone, Deserialize, Serialize, Debug)]
@@ -23,10 +50,9 @@ struct Post {
     content: String,
 }
 
-
 #[derive(Clone, Serialize, Debug)]
 struct Index<'a> {
-    posts: &'a Vec<Post>
+    posts: &'a Vec<Post>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -45,6 +71,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let i = fs::read_to_string("./bisenum/index.html")?;
     tt.add_template("index", &i)?;
 
+    let t = fs::read_to_string("./bisenum/tag.html")?;
+    tt.add_template("tag", &t)?;
 
     let mut options = ComrakOptions::default();
     options.extension.strikethrough = true;
@@ -59,7 +87,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut posts: Vec<Post> = Vec::new();
     for path in paths {
         let mut b: Post = serde_dhall::from_file(path.unwrap().path()).parse()?;
-        b.content = markdown_to_html(&b.content, &ComrakOptions::default());
+        b.content = markdown_to_html(&b.content, &options);
         posts.push(b);
     }
 
@@ -68,9 +96,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     fs::write(format!("{}/index.html", build_dir), rendered_index)?;
 
     fs::create_dir(format!("{}/gewritu", &build_dir))?;
-    for post in posts {
+    for post in &posts {
         let rendered = tt.render("post", &post)?;
-        fs::write(format!("{}/gewritu/{}.html", build_dir, post.slug), rendered)?;
+        fs::write(
+            format!("{}/gewritu/{}.html", build_dir, post.slug),
+            rendered,
+        )?;
+    }
+
+    fs::create_dir(format!("{}/tags", &build_dir))?;
+    for tag in Tag::iterator() {
+        let tag_page = TagPage {
+            tag: tag.clone(),
+            posts: posts
+                .clone()
+                .into_iter()
+                .filter(|p| p.tags.contains(tag))
+                .collect(),
+        };
+        let rendered_tag = tt.render("tag", &tag_page)?;
+        fs::write(format!("{}/tags/{}.html", build_dir, tag), rendered_tag)?;
     }
 
     Ok(())
